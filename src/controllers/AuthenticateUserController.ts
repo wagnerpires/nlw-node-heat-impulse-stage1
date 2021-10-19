295b99783b6c5f55dd5ce5681578ba0490be7d38
Wagner Pires <wagnerpires@hotmail.com>
2021-10-19 16:18:04 -0700

    2 day

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -4,8 +4,12 @@ import { AuthenticateUserService } from "../services/AuthenticateUserService";
 class AuthenticateUserController {
     async handle (request: Request, response: Response) {
         
+        const { code } = request.body;
+
         const service = new AuthenticateUserService();
-        // service.execute("121212");
+        const result = await service.execute(code);
+
+        return response.json(result);
     }
 }
 
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 /*
 * Receber code(string)
 * Recuperar o access_token no github
@@ -9,9 +10,21 @@
 
 class AuthenticateUserService {
     async execute(code: string) {
+        const url = "https://github.com/login/oauth/access_token";
+        const response = await axios.post(url, null, {
+            params: {
+                client_id: process.env.GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code,
+            },
+            headers:  {
+                "Accept": "application/json"
+            }
+        })
+
+        return response.data;
         
     }
-
 }
 
 export { AuthenticateUserService }
